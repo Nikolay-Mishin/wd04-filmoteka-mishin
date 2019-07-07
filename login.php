3605fe88e4af9f8bfe5b8b2631db9964231095ff
Николай Мишин <NIKMIS@live.spbstu.ru>
2019-07-07 22:22:28 +0300

    lesson 9 added

---
+++ login.php
@@ -0,0 +1,33 @@
+<?php 
+require('config.php');
+require('database.php');
+$link = db_connect();
+
+require('functions/login-functions.php');
+
+if ( isset($_POST['enter']) ) {
+	$userName = 'admin';
+	$userPassword = '123456';
+
+    $user = get_user($link, 'admin');
+    /* 
+        echo "<pre>";
+        print_r($user);
+        echo "</pre>"; 
+    */
+
+	if ( $_POST['login'] == $user['user_name'] ) {
+		if ( $_POST['password'] == $user['user_password'] ) {
+
+			$_SESSION['user'] = $user['user_name'];
+			// header('Location: ' . HOST . 'index.php');
+            header('Location: ' . 'index.php');
+		}
+	}
+
+}
+
+include('views/head.tpl');
+include('views/login.tpl');
+include('views/footer.tpl');
+?>
